a5c0356bea4a129f1f796cd9ed2abe6fee4d4dfa
Sach-Singh <128513867+Sach-Singh@users.noreply.github.com>
2023-03-22 23:33:40 +0530

    Add files via upload

---
+++ Annotations/Annotations/src/com/ani/anno/Account.java
@@ -0,0 +1,15 @@
+package com.ani.anno;
+
+public class Account {
+    @GeneratorAccessors
+    private String ac;
+    // public void getAc(String ac){
+    //     ac = this.ac;
+    // }
+
+    @GeneratorAccessors
+    private double balance;
+    // public void getBalance(double balance){
+    //     balance = this.balance;
+    // }
+}
