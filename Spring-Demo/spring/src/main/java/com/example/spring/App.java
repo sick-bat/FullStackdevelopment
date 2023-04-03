d292c595a3279cadd6b4afab2ebeddd358698a58
Sach-Singh <sach.s0501@gmail.com>
2023-04-03 13:10:25 +0530

    excel

--- Spring-Demo/spring/src/main/java/com/example/spring/App.java
+++ Spring-Demo/spring/src/main/java/com/example/spring/App.java
@@ -1,29 +1,57 @@
 package com.example.spring;
 
+import java.util.Arrays;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import com.example.spring.config.AppConfig;
-import com.example.spring.util.FileChecker;
-import com.example.spring.util.FileUtil;
+import com.ani.spring.config.AppConfig;
+import com.ani.spring.excel.XlConfig;
+import com.ani.spring.util.FileChecker;
+import com.ani.spring.util.FileUtil;
 
 public final class App {
 
     public static void normal() {
+        System.out.println("🔥 Providing Dependencies Normally 🔥");
+
         FileUtil util = new FileUtil();
 
         FileChecker checker = new FileChecker(util);
         checker.checkFileValidity();
     }
 
-    public static void withSpring() {
+    public static void withSpringJavaConfig() {
+        System.out.println("🔥 Providing Dependencies With Java Code 🔥");
         ApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        String[] names = ctx.getBeanDefinitionNames();
+        for(String nm : names) {
+            System.out.println(" 👉 "+nm);
+        }
 
         FileChecker checker = ctx.getBean(FileChecker.class);
         checker.checkFileValidity();
     }
 
+    public static void withSpringXmlConfig() {
+        System.out.println("🔥 Providing Dependencies With Xml Config 🔥");
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("beans.xml");
+
+        FileChecker checker = ctx.getBean(FileChecker.class);
+        checker.checkFileValidity();
+    }
+
+    public static void withSpringAnnotationConfig() {
+        System.out.println("🔥 Providing Dependencies With Annotation Config 🔥");
+        ApplicationContext ctx = new AnnotationConfigApplicationContext(XlConfig.class, AppConfig.class);
+
+        String[] names = ctx.getBeanDefinitionNames();
+        System.out.println(Arrays.toString(names));
+    }
+
     public static void main(String[] args) {
-        withSpring();
+        withSpringAnnotationConfig();
     }
 }
--- Spring-Demo/spring/src/main/java/com/example/spring/config/AppConfig.java
+++ Spring-Demo/spring/src/main/java/com/example/spring/config/AppConfig.java
@@ -1,21 +1,20 @@
 package com.example.spring.config;
 
-
 import java.io.File;
 
 import org.springframework.context.annotation.Bean;
 
-import com.example.spring.util.FileChecker;
-import com.example.spring.util.FileUtil;
+import com.ani.spring.util.FileChecker;
+import com.ani.spring.util.FileUtil;
 
 public class AppConfig {
    
-    @Bean
+    @Bean // lifecyle of this FileUtil would be handled by spring IoC container
     public FileUtil util() {
         return new FileUtil();
     }
 
-    @Bean
+    @Bean // lifecyle of this FileChecker along with dependency FileUtil, would be handled by Spring IoC container
     public FileChecker checker(FileUtil util) {
         return new FileChecker(util);
     }
